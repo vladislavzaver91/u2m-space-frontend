99ecea546d9c760dc91f36b6e3cfa565b3480f65
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-04-30 21:00:19 +0300

    feat: updated classifieds data

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ['via.placeholder.com'],
+		domains: ['developers.elementor.com'],
 	},
 }
 

7718879b1cbc53072885dba12e33bde9999d7bfc
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-05-02 23:11:53 +0300

    fix: fixed default user avatar

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,29 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ['developers.elementor.com'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: '*.googleusercontent.com',
+				pathname: '**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'u2m-space-backend-production.up.railway.app',
+				pathname: '/public/**',
+			},
+		],
+		domains: [
+			'developers.elementor.com',
+			'lh3.googleusercontent.com',
+			'googleusercontent.com',
+			'u2m-space-backend-production.up.railway.app',
+		],
 	},
 }
 

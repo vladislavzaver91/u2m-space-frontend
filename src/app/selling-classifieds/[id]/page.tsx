a412ae87600a88d485a83513157c0ffe2c14e0f3
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-05-19 21:01:53 +0300

    fix: fixed loader

--- src/app/selling-classifieds/[id]/page.tsx
+++ src/app/selling-classifieds/[id]/page.tsx
@@ -176,7 +176,7 @@ export default function ClassifiedDetail() {
 
 	if (isLoading) {
 		return (
-			<div className='min-h-full flex flex-col items-center justify-center'>
+			<div className='min-h-screen flex flex-col items-center justify-center'>
 				<Loader />
 			</div>
 		)

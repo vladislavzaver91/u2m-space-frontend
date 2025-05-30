313cb7757f4d6afd5969a5d9d0581568278cff4c
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-05-30 11:02:52 +0300

    fix: fixed locale prefix on LoginModal

--- src/components/login-modal.tsx
+++ src/components/login-modal.tsx
@@ -35,7 +35,7 @@ export const LoginModal = () => {
 		{
 			icon: '/icons/google.svg',
 			name: 'Google',
-			href: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google??locale=${locale}?prompt=select_account`,
+			href: `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google?prompt=select_account?locale=${locale}`,
 		},
 	]
 
